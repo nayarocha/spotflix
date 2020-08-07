85563e9cf02e45d6aeee27ae35695196e60423e3
NayaRocha <nayaarocha@gmail.com>
2020-08-07 10:16:18 -0300

    Inserindo bannerMain

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import Menu from './components/Menu/index.js';
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
  
 function App() {
   return (
-    <Menu/>
+    <div>
+      <Menu/>
+
+      <BannerMain
+        videoTitle = {dadosIniciais.categorias[0].videos[0].titulo}
+        url = {dadosIniciais.categorias[0].videos[0].url}
+        videoDescription = {"Testando video Description"}
+      
+      />
+    </div>
+    
+
+    
     
   );
 }
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -12,7 +12,7 @@ function Menu(){
             </a>
 
             <Button as="a" className="ButtonLink" href="/" >
-                Novo vídeo agora
+                Novo vídeo
             </Button>
 
         </nav>
