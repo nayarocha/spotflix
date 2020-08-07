ad0bded553a9be76273c771571362ec83efbad06
NayaRocha <nayaarocha@gmail.com>
2020-08-06 23:49:12 -0300

    teste

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -12,7 +12,7 @@ function Menu(){
             </a>
 
             <Button as="a" className="ButtonLink" href="/" >
-                Novo vídeo
+                Novo vídeo agora
             </Button>
 
         </nav>
